dc28155df5b8caf102acc0393c9f604c95d13ed7
gilad <gmoav8@gmail.com>
2025-06-17 15:35:48 +0300

    Add bumblebee

--- proj1/prime.cpp
+++ proj1/prime.cpp
@@ -12,3 +12,20 @@ bool isPrime(unsigned int number) {
     }
     return true;
 }
+
+/** 
+ * find 'count' primes and put them in 'output_array'
+ * output_array [OUT] pointer to an unsigned integer array to put the results in.
+ * count [IN] the number of primes to find.
+ */
+void find_primes(unsigned int output_array[], unsigned int count) {
+    unsigned int current_count = 0;
+    unsigned int current_number = MINIMAL_DIVIDER;
+
+    while (current_count < count) {
+        if (is_prime(current_number)) {
+            output_array[current_count++] = current_number;
+        }
+        current_number++;
+    }
+}
--- proj1/prime.h
+++ proj1/prime.h
@@ -4,5 +4,6 @@
 constexpr unsigned int MINIMAL_DIVIDER = 2;
 
 bool isPrime(unsigned int number);
+void find_primes(unsigned int output_array[], unsigned int count);
 
 #endif // !__PRIME_H__
