e54471d9d32bac7625613f7ddf0e8a2e41bc731a
gilad <gmoav8@gmail.com>
2025-06-17 18:10:12 +0300

    More CR changes

--- proj1/groot.cpp
+++ proj1/groot.cpp
@@ -1,39 +1,24 @@
-
 #include <iostream>
-#include <regex>
-#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::regex;
-using std::regex_match;
 using std::sqrt;
-using std::stoul;
-using std::string;
-using std::out_of_range;
 
 constexpr int ERROR_RETURN_VALUE = -1;
-const regex NUMBER_REGEX("\\d+");
+constexpr int GOOD_RETURN_VALUE = 0;
 
 int main() {
     cout << "Please provide an integer: ";
 
-    string input;
+    unsigned long input;
     cin >> input;
 
-    if (!regex_match(input, NUMBER_REGEX)) {
+    if (!cin) {
         cout << "Make sure the input is a valid positive integer!" << endl;
         return ERROR_RETURN_VALUE;
     }
 
-    unsigned long input_long;
-    try {
-        input_long = stoul(input);
-    } catch (out_of_range) {
-        cout << "Make sure the input is in range!" << endl;
-        return ERROR_RETURN_VALUE;
-    }
-
-    cout << "Sqrt is: " << sqrt(input_long) << endl;
+    cout << "Sqrt is: " << sqrt(input) << endl;
+    return GOOD_RETURN_VALUE;
 }
