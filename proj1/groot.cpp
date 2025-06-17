fdbaee35059974bee6cf4249f4c6d556a43606a4
gilad <gmoav8@gmail.com>
2025-06-17 12:42:16 +0300

    Make sure curly brackets are on the same line

--- proj1/groot.cpp
+++ proj1/groot.cpp
@@ -3,19 +3,17 @@
 #include <regex>
 #include <string>
 
-int main()
-{
+int main() {
     std::string input;
     std::regex number_regex("\\d+");
-    
+
     std::cout << "Please provide an integer: ";
     std::cin >> input;
 
-    if (!std::regex_match(input, number_regex))
-    {
+    if (!std::regex_match(input, number_regex)) {
         std::cout << "Make sure the input is a valid positive integer!" << std::endl;
         return -1;
     }
 
-    std::cout << "Sqrt is: " << std::sqrt(std::stoi(input))<< std::endl;
+    std::cout << "Sqrt is: " << std::sqrt(std::stoi(input)) << std::endl;
 }
