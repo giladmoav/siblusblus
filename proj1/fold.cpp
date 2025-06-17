46fed218d28d32f8e2ce92c759872ed593a4af65
gilad <gmoav8@gmail.com>
2025-06-17 14:47:18 +0300

    CR changes

--- proj1/fold.cpp
+++ proj1/fold.cpp
@@ -1,22 +1,31 @@
 
 #include <iostream>
 
-constexpr int BOARD_SIZE = 11;
+constexpr unsigned int BOARD_SIZE = 11;
+
+using std::cout;
+using std::endl;
+
+/// print headers of product table of size 'board_size'
+void print_column_headers(unsigned int board_size) {
+    for (int i = 1; i <= board_size; i++) {
+        cout << "\t" << i;
+    }
+    cout << endl;
+}
+
+/// print rows of product table of size 'board_size'
+void print_rows(unsigned int board_size) {
+    for (int i = 1; i <= board_size; i++) {
+        cout << i;
+        for (int j = 1; j <= board_size; j++) {
+            cout << "\t" << i * j;
+        }
+        cout << endl;
+    }
+}
 
 void main() {
-
-    // print columns' headers
-    for (int i = 1; i <= BOARD_SIZE; i++) {
-        std::cout << "\t" << i;
-    }
-    std::cout << std::endl;
-
-    // print rows
-    for (int i = 1; i <= BOARD_SIZE; i++) {
-        std::cout << i;
-        for (int j = 1; j <= BOARD_SIZE; j++) {
-            std::cout << "\t" << i * j;
-        }
-        std::cout << std::endl;
-    }
+    print_column_headers(BOARD_SIZE);
+    print_rows(BOARD_SIZE);
 }
