ee8aae8540c5d0aa108def60cba773a2093caa37
gilad <gmoav8@gmail.com>
2025-06-17 17:14:48 +0300

    CR changes 2

--- proj1/main.cpp
+++ proj1/main.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include "prime.h"
+#include <iostream>
 
 using std::cout;
+using std::endl;
 
 int main() {
-    cout << "9 is prime? " << is_prime(9) << std::endl;
-    cout << "11 is prime? " << is_prime(11) << std::endl;
-    cout << "13 is prime? " << is_prime(13) << std::endl;
-    cout << "16 is prime? " << is_prime(16) << std::endl;
+    cout << "9 is prime? " << is_prime(9) << endl;
+    cout << "11 is prime? " << is_prime(11) << endl;
+    cout << "13 is prime? " << is_prime(13) << endl;
+    cout << "16 is prime? " << is_prime(16) << endl;
 }
--- proj1/prime.cpp
+++ proj1/prime.cpp
@@ -1,9 +1,11 @@
-#include <cmath>
 #include "prime.h"
+#include <cmath>
 
 /// returns true if 'number' is prime
 bool is_prime(unsigned int number) {
-    for (int i = MINIMAL_DIVIDER; i <= std::sqrt(number); i++) {
+    long double long_double_number = static_cast<long double>(number);
+    int maximal_divider = static_cast<int>(std::sqrt(long_double_number));
+    for (int i = MINIMAL_DIVIDER; i <= maximal_divider; i++) {
         if (number % i == 0) {
             return false;
         }
