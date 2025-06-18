2f86a2fb5cb3448ec597299ab3fc1c12dc43a786
gilad <gmoav8@gmail.com>
2025-06-18 11:18:45 +0300

    Change ambigous "GOOD" to "SUCCESS"

--- proj1/groot.cpp
+++ proj1/groot.cpp
@@ -5,8 +5,8 @@ using std::cout;
 using std::endl;
 using std::sqrt;
 
-constexpr int ERROR_RETURN_VALUE = -1;
-constexpr int GOOD_RETURN_VALUE = 0;
+constexpr int ERROR_RETURN_CODE = -1;
+constexpr int SUCCESS_RETURN_CODE = 0;
 
 int main() {
     cout << "Please provide an integer: ";
@@ -16,9 +16,9 @@ int main() {
 
     if (!cin) {
         cout << "Make sure the input is a valid positive integer!" << endl;
-        return ERROR_RETURN_VALUE;
+        return ERROR_RETURN_CODE;
     }
 
     cout << "Sqrt is: " << sqrt(input) << endl;
-    return GOOD_RETURN_VALUE;
+    return SUCCESS_RETURN_CODE;
 }
