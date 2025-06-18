5a87d3330fe07ad73aa59cd2057a0ffb268b8077
gilad <gmoav8@gmail.com>
2025-06-18 10:45:27 +0300

    CR changes

--- proj1/prime.h
+++ proj1/prime.h
@@ -4,7 +4,7 @@
 constexpr unsigned int MINIMAL_DIVIDER = 2;
 
 bool isPrime(unsigned int number);
-void find_primes(unsigned int output_array[], unsigned int count);
+void findPrimes(unsigned int output_array[], unsigned int count);
 
 #endif // !__PRIME_H__
 
